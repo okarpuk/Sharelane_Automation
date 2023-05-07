937b0593fe44ea290144545c7955bf48b9540f3a
okarpuk <oleg.karpuk.1982@gmail.com>
2023-05-07 19:23:43 +0300

    Utilites added

---
+++ Sharelane_Automation/Core/Utilites/UserBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Core.Utilites
+{
+    public static class UserBuilder
+    {
+        public static User StandartUser => new User
+        {
+            Name = TestContext.Parameters.Get("UserName"),
+            Password = TestContext.Parameters.Get("UserPassword"),
+        };
+    }
+}

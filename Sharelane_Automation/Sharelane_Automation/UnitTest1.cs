7aa27086caed6d957d4c6bbb132d02d29eadb6cf
okarpuk <oleg.karpuk.1982@gmail.com>
2023-05-07 14:29:14 +0300

    Add project files.

---
+++ Sharelane_Automation/Sharelane_Automation/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Sharelane_Automation
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
